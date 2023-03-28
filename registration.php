d8e40b837bf0a8aaaa31a7e480dfb0778a4d7254
Petr Vorlíček <petrvorlicek7@gmail.com>
2023-03-28 16:08:03 +0200

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="login.php" method="post">
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email">
+        <label for="password">Heslo:</label>
+        <input type="password" name="password" id="password">
+        <label for="chechPassword">Check password:</label>
+        <input type="password" name="chechPassword" id="chechPassword">
+    </form>
+</body>
+</html>
