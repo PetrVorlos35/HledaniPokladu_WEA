2cac22291fb96d09bad3832454e125dc3b5cdeab
Petr Vorlíček <petrvorlicek7@gmail.com>
2023-03-29 13:11:09 +0200

    Update login.php

--- login.php
+++ login.php
@@ -5,25 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Přihlášení</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <form action="index.php" method="post">
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email">
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password">
-        <input type="submit" value="Login!">
-    </form>
-
-    <form action="index.php" method="post">
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email">
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password">
-        <label for="password2">Password again:</label>
-        <input type="password" name="password2" id="password2">
-        <input type="submit" value="Register!">
-    </form>
+    <div id="login">
+        <form action="index.php" method="post">
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email">
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password">
+            <input type="submit" value="Login!">
+        </form>
+    </div>
+<br><br><br><br><br><br>
+    <div id="register">
+        <form action="index.php" method="post">
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email">
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password">
+            <label for="password2">Password again:</label>
+            <input type="password" name="password2" id="password2">
+            <input type="submit" value="Register!">
+        </form>
+    </div>
 </body>
 </html>
