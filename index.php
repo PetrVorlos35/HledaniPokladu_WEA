8b963c1f40ba4cf31058fa93385426893f30ffa5
Petr Vorlíček <petrvorlicek7@gmail.com>
2023-03-30 13:05:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hledání pokladu</title>
-    <link rel="stylesheet" href="styles.css">
     <script
     src="https://code.jquery.com/jquery-3.6.3.min.js"
     integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU="
     crossorigin="anonymous"></script>
+    <!-- <link rel="stylesheet" href="styles.css"> -->
+    <link rel="stylesheet" href="styles.css">
 </head>
-<body>
+<body style="background-color: rgb(40, 44, 52); color:white">
     <label for="sirka">Šířka:</label>
     <input type="number" name="sirka" id="sirka" min="4" max="20" value="10">
     <label for="vyska">Výška:</label>
@@ -33,9 +34,11 @@
             
             var sirka = $("#sirka").val();
             var vyska = $("#vyska").val();
+            let id = 1;
             for (let i = 0; i < vyska; i++) {
                 for (let j = 0; j < sirka; j++) {
-                    gamePool.append("<button class='box' onclick='checkBox()'>X</button>");
+                    gamePool.append("<button id='button-" + id + "' class='box' onclick='checkBox()'>X</button>");
+                    id++;
                 }
                 gamePool.append("<br>");
             }
