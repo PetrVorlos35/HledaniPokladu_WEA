e64d1518e5ad19bd3d03cf172cbdf9b443397752
Petr Vorlíček <petrvorlicek7@gmail.com>
2023-04-19 17:24:14 +0200

    Update stats.php

--- stats.php
+++ stats.php
@@ -1,3 +1,23 @@
+<?php
+    $connect = new mysqli("localhost", "root","","poklad_wea") or die();
+
+    $email = $_SESSION["email"];
+    $sql = "SELECT * FROM users WHERE email = '$email'";
+    $result = $connect->query($sql);
+    $row = mysqli_fetch_assoc($result);
+    $user_id = $row["id"];
+    $game_size = $_POST["velikostPole"];
+    $moves_count = $_POST["pokusy"];
+
+    $sql = "INSERT INTO stats (user_id, game_size, moves_count) VALUES ('$user_id', '$game_size', '$moves_count')";
+    if ($result = $connect->query($sql)) {
+        echo "Stats saved!";
+    }else {
+        echo "error!";
+    }
+
+    mysqli_close($connect);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
