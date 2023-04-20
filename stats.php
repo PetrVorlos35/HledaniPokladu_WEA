e9d98e54bcecce5a422f8ac3a2fcb4e95d784a48
Petr Vorlíček <petrvorlicek7@gmail.com>
2023-04-20 21:36:57 +0200

    Update stats.php

--- stats.php
+++ stats.php
@@ -1,15 +1,17 @@
 <?php
+    session_start();
     $connect = new mysqli("localhost", "root","","poklad_wea") or die();
 
     $email = $_SESSION["email"];
-    $sql = "SELECT * FROM users WHERE email = '$email'";
-    $result = $connect->query($sql);
+    $get_id = "SELECT * FROM users WHERE email = '$email'";
+    $result = $connect->query($get_id);
     $row = mysqli_fetch_assoc($result);
     $user_id = $row["id"];
     $game_size = $_POST["velikostPole"];
     $moves_count = $_POST["pokusy"];
+    $id = $connect->query("SELECT MAX(id) FROM stats")->fetch_row()[0] + 1;
 
-    $sql = "INSERT INTO stats (user_id, game_size, moves_count) VALUES ('$user_id', '$game_size', '$moves_count')";
+    $sql = "INSERT INTO stats VALUES ('$id', '$user_id', '$game_size', '$moves_count')";
     if ($result = $connect->query($sql)) {
         echo "Stats saved!";
     }else {
