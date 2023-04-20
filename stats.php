9c656b0b95a3ce291572e3f21d28e30ceb05ad62
Petr Vorlíček <petrvorlicek7@gmail.com>
2023-04-20 22:42:21 +0200

    Update stats.php

--- stats.php
+++ stats.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    $connect = mysqli_connect("localhost", "root", "", "poklad_wea");
+    $connect = mysqli_connect("localhost", "root", "", "poklad_wea") or die();
     if (!$connect) {
         die("Connection failed: " . mysqli_connect_error());
     }
